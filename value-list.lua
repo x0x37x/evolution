a9a2bd1969fd10875d808069c17e25f10ccee6a1
Dylan <138546622+x0x37x@users.noreply.github.com>
2024-10-17 12:46:27 +1300

    Create value-list.lua

---
+++ value-list.lua
@@ -0,0 +1,86 @@
+return {
+    Oak = {
+        Log = 9,
+        Plank = 106
+    },
+    Walnut = {
+        Log = 9,
+        Plank = 106
+    },
+    Cherry = {
+        Log = 9,
+        Plank = 106
+    },
+    SnowGlow = {
+        Log = 9,
+        Plank = 106
+    },
+    Pink = {
+        Log = 1.5,
+        Plank = 10
+    },
+    Generic = {
+        Log = 1.5,
+        Plank = 10
+    },
+    Birch = {
+        Log = 2.25,
+        Plank = 15
+    },
+    Koa = {
+        Log = 2.8,
+        Plank = 26,    
+    },
+    Test = {
+        Log = 3,
+        Plank = 5.8,
+    },
+    Fir = {
+        Log = 3.2,
+        Plank = 18
+    },
+    Pine = {
+        Log = 3.2,
+        Plank = 18
+    },
+    Volcano = {
+        Log = 3.5,
+        Plank = 28,
+    },
+    GreenSwampy = {
+        Log = 4.4,
+        Plank = 30,
+    },
+    CaveCrawler = {
+        Log = 5.1,
+        Plank = 35
+    },
+    Palm = {
+        Log = 5.6,
+        Plank = 32,
+    },
+    GoldSwampy = {
+        Log = 5.7,
+        Plank = 36
+    },
+    Frost = {
+        Log = 9,
+        Plank = 106
+    },
+    Spooky = {
+        Log = 19,
+        Plank = 52
+    },
+    SpookyNeon = {
+        Log = 25,
+        Plank = 90
+    },
+    Sign = {
+        Log = 170,
+        Plank = 170,
+    },
+    LoneCave = {
+        Log = 150,
+        Plank = 420
+    }
+}
