9129b43d9a2f4103f0139b3a7954a5bcbd3479c6
Dylan <138546622+x0x37x@users.noreply.github.com>
2024-11-04 11:06:55 +1300

    Create axes.lua

---
+++ axes.lua
@@ -0,0 +1,193 @@
+return {
+    ["AxeClass_CaveAxe"] = {
+        ["SwingCooldown"] = 0.4,
+        ["Range"] = 10,
+        ["SpecialTrees"] = {
+            ["CaveCrawler"] = {
+                ["SwingCooldown"] = 0.22,
+                ["Damage"] = 7.2
+            }
+        },
+        ["Damage"] = 0.4
+    },
+    ["AxeClass_AxeChicken"] = {
+        ["SwingCooldown"] = 0.3,
+        ["Range"] = 10,
+        ["Damage"] = 0.9
+    },
+    ["AxeClass_Rukiryaxe"] = {
+        ["SwingCooldown"] = 0.4,
+        ["Range"] = 9,
+        ["Damage"] = 1.68
+    },
+    ["AxeClass_Axe1"] = {
+        ["SwingCooldown"] = 0.73,
+        ["Range"] = 10,
+        ["Damage"] = 0.55
+    },
+    ["AxeClass_BasicHatchet"] = {
+        ["SwingCooldown"] = 0.65,
+        ["Range"] = 10,
+        ["Damage"] = 0.2
+    },
+    ["AxeClass_SilverAxe"] = {
+        ["SwingCooldown"] = 0.48,
+        ["Range"] = 10,
+        ["Damage"] = 1.6
+    },
+    ["AxeClass_AxeSwamp"] = {
+        ["SwingCooldown"] = 0.55,
+        ["Range"] = 9,
+        ["SpecialTrees"] = {
+            ["GoldSwampy"] = {
+                ["SwingCooldown"] = 0.35,
+                ["Damage"] = 5.35
+            },
+            ["GreenSwampy"] = {
+                ["SwingCooldown"] = 0.46,
+                ["Damage"] = 7
+            }
+        },
+        ["Damage"] = 0.8
+    },
+    ["AxeClass_CandyCaneAxe"] = {
+        ["SwingCooldown"] = 0.7,
+        ["Range"] = 6,
+        ["Damage"] = 0
+    },
+    ["AxeClass_CandyCornAxe"] = {
+        ["SwingCooldown"] = 0.6,
+        ["Range"] = 16,
+        ["Damage"] = 1.75
+    },
+    ["AxeClass_AxeAlphaTesters"] = {
+        ["SwingCooldown"] = 0.5,
+        ["Range"] = 10,
+        ["Damage"] = 1.5
+    },
+    ["AxeClass_AxeBetaTesters"] = {
+        ["SwingCooldown"] = 0.54,
+        ["Range"] = 10,
+        ["Damage"] = 1.45
+    },
+    ["AxeClass_AxePie"] = {
+        ["SwingCooldown"] = 0.3,
+        ["Range"] = 15.5,
+        ["SpecialTrees"] = {
+            ["Cherry"] = {
+                ["SwingCooldown"] = 0.25,
+                ["Damage"] = 1.9
+            }
+        },
+        ["Damage"] = 0.95
+    },
+    ["AxeClass_BluesteelAxe"] = {
+        ["SwingCooldown"] = 0.8,
+        ["Range"] = 12,
+        ["SpecialTrees"] = {
+            ["BlueSpruce"] = {
+                ["SwingCooldown"] = 0.68,
+                ["Damage"] = 12.1
+            }
+        },
+        ["Damage"] = 2.8
+    },
+    ["AxeClass_FireAxe"] = {
+        ["SwingCooldown"] = 0.55,
+        ["Range"] = 9,
+        ["SpecialTrees"] = {
+            ["Volcano"] = {
+                ["SwingCooldown"] = 0.35,
+                ["Damage"] = 6.35
+            }
+        },
+        ["Damage"] = 0.6
+    },
+    ["AxeClass_IceAxe"] = {
+        ["SwingCooldown"] = 0.4,
+        ["Range"] = 9.2,
+        ["SpecialTrees"] = {
+            ["Frost"] = {
+                ["SwingCooldown"] = 0.31,
+                ["Damage"] = 6
+            }
+        },
+        ["Damage"] = 0.36
+    },
+    ["AxeClass_Beesaxe"] = {
+        ["SwingCooldown"] = 0.5,
+        ["Range"] = 13,
+        ["Damage"] = 1.4
+    },
+    ["AxeClass_Axe3"] = {
+        ["SwingCooldown"] = 0.65,
+        ["Range"] = 10,
+        ["Damage"] = 1.45
+    },
+    ["AxeClass_Axe2"] = {
+        ["SwingCooldown"] = 0.7,
+        ["Range"] = 10,
+        ["Damage"] = 0.93
+    },
+    ["AxeClass_GingerbreadAxe"] = {
+        ["SwingCooldown"] = 0.5,
+        ["Range"] = 32,
+        ["SpecialTrees"] = {
+            ["Walnut"] = {
+                ["SwingCooldown"] = 0.4,
+                ["Damage"] = 8.5
+            },
+            ["Koa"] = {
+                ["SwingCooldown"] = 0.4,
+                ["Damage"] = 11
+            }
+        },
+        ["Damage"] = 1.2
+    },
+    ["AxeClass_ManyAxe"] = {
+        ["SwingCooldown"] = 1.9,
+        ["Range"] = 16,
+        ["Damage"] = 10.2
+    },
+    ["AxeClass_AxeAmber"] = {
+        ["SwingCooldown"] = 1,
+        ["Range"] = 9,
+        ["Damage"] = 3.39
+    },
+    ["AxeClass_MintAxe"] = {
+        ["SwingCooldown"] = 5,
+        ["Range"] = 1,
+        ["Damage"] = 0.8
+    },
+    ["AxeClass_RustyAxe"] = {
+        ["SwingCooldown"] = 0.4,
+        ["Range"] = 10,
+        ["Damage"] = 0.55
+    },
+    ["AxeClass_AxeTwitter"] = {
+        ["SwingCooldown"] = 0.4,
+        ["Range"] = 16,
+        ["SpecialTrees"] = {
+            ["CaveCrawler"] = {
+                ["SwingCooldown"] = 0.4,
+                ["Damage"] = 3.9
+            },
+            ["Volcano"] = {
+                ["SwingCooldown"] = 0.4,
+                ["Damage"] = 2.5
+            }
+        },
+        ["Damage"] = 1.65
+    },
+    ["AxeClass_EndTimesAxe"] = {
+        ["SwingCooldown"] = 0.4,
+        ["Range"] = 9,
+        ["SpecialTrees"] = {
+            ["LoneCave"] = {
+                ["SwingCooldown"] = 0.3,
+                ["Damage"] = 10000000
+            }
+        },
+        ["Damage"] = 1.58
+    }
+}
